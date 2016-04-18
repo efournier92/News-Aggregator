bbf70bf3028aedede8a3902a5c0305592d28ee46
Misigno <efournier92@gmail.com>
2016-04-18 18:51:16 -0400

    Initial Commit

---
+++ models/article.rb
@@ -0,0 +1,16 @@
+class Article
+
+  attr_reader :description, :title, :url
+
+  def initialize(title, url, description)
+    @description = description
+    @title       = title
+    @url         = url
+  end
+
+  def lister
+    File.open('articles.csv', 'a+') do |article|
+      puts article[0]
+    end
+  end
+end
