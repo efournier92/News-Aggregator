61669ddb7d216d92fa91e5586aadcbd524c712aa
Misigno <efournier92@gmail.com>
2016-05-09 17:16:51 -0400

    update README

--- schema.sql
+++ schema.sql
@@ -1,7 +1,3 @@
--- If you want to run this schema repeatedly you'll need to drop
--- the table before re-creating it. Note that you'll lose any
--- data if you drop and add a table:
-
 DROP TABLE IF EXISTS articles;
 
 CREATE TABLE articles (
